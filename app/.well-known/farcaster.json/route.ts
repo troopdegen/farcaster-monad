d7db587d22e75e9763f5ee16b12aaf02f91914eb
troopdegen <mel@innvertir.com>
2025-08-20 12:44:01 -0600

    update farcaster manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,6 +4,14 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
     // TODO: Add your own account association
+    // example association for workshops:
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjEwNjg1LCJ0eXBlIjoiYXV0aCIsImtleSI6IjB4ODI1Nzk0ZjM3OTJlODE5NTFGRTU1QjdGNTI3ZWFiN2U4MDYzZDJjYyJ9",
+      payload: "eyJkb21haW4iOiJmYXJjYXN0ZXItbW9uYWQudmVyY2VsLmFwcCJ9",
+      signature:
+        "/RNouvX/hJ55JqDX6ZyEgX/WIncamhQzWWVCbpWs16xdxsFIUuQMD0yX7xRjUhNXms5usWa1ghevrYyvkQYstBw=",
+    },
     frame: {
       version: "1",
       name: "Monad Farcaster MiniApp Template",
